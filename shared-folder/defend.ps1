433a1df95e2595c55184776dbe3ab9559af27b7d
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2023-08-10 16:34:14 +1000

    Improved testInternetAccess()
    Added if statement to output user friendly output to the command prompt

--- shared-folder/defend.ps1
+++ shared-folder/defend.ps1
@@ -4,7 +4,14 @@ function testInternetAccess()
 #can use -UseBasicParsing | Select-Object StatusCode to display its status code 200 means OK
 #I used the website ipinfo.io/ip to check internet connection, it is a simple service that returns
 #the ip address of the request and has high avaliability
-Invoke-WebRequest -Uri https://ipinfo.io/ip -UseBasicParsing | Select-Object StatusCode
+$webRespons = Invoke-WebRequest -Uri https://ipinfo.io/ip -UseBasicParsing | Select-Object StatusCode
+if($webResponse = 200)
+{
+echo Web response 200 iternet connection successful 
+}else
+{
+
+}
 }
 
 #run testInternetAccess function
