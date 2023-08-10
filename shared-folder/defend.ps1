0606e2dcb8528dbe88444126a0337757226ed6f3
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2023-08-10 16:43:15 +1000

    Comments
    Added comments

--- shared-folder/defend.ps1
+++ shared-folder/defend.ps1
@@ -6,13 +6,15 @@ function testInternetAccess()
 #the ip address of the request and has high avaliability
 $webResponse = Invoke-WebRequest -Uri https://ipinfo.io/ip
 
-
+#checks the response for the flag StatusCode and if its equal to 200 returns internet connection successfull
 if($webResponse.StatusCode -eq 200)
 {
 echo "Web response 200 iternet connection successful"
  
 }else
 {
+#if any other response assumes the internet is down and will attempt to communicate with a alternate server
+#to check if the server is down or if the internet is definitly down.
 echo "web response returned $webRespons"
 echo "web response failed attempting alternate server"
 }
