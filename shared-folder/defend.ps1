e9a813fbafdbcc6a6e9d5e84f4c8832cf58cd571
chromatic-wolf <34954828+chromatic-wolf@users.noreply.github.com>
2023-08-10 16:28:42 +1000

    Added comments
    added comments explaining command and choice of service

--- shared-folder/defend.ps1
+++ shared-folder/defend.ps1
@@ -1,6 +1,9 @@
 function testInternetAccess()
 {
-
+#invoke-webRequest sends a web request and returnes the response
+#can use -UseBasicParsing | Select-Object StatusCode to display its status code 200 means OK
+#I used the website ipinfo.io/ip to check internet connection, it is a simple service that returns
+#the ip address of the request and has high avaliability
 Invoke-WebRequest -Uri https://ipinfo.io/ip -UseBasicParsing | Select-Object StatusCode
 }
 
