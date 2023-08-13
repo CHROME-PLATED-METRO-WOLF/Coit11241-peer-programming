0a5278894e97f34af77f53bcd71e87b42dc57671
Caleb <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2023-08-14 00:10:05 +1000

    Update echo to Write-Output
    Changed echo to Write-Output as per script-analyzer output

--- shared-folder/defend.ps1
+++ shared-folder/defend.ps1
@@ -15,7 +15,7 @@ function testInternetAccess()
     {
         #if any other response assumes the internet is down and will attempt to communicate with a alternate server
         #to check if the server is down or if the internet is definitly down.
-        echo "web response failed attempting alternate server"
+        Write-Output "web response failed attempting alternate server"
         $webResponse = Invoke-WebRequest -Uri www.google.com
         if($webResponse.StatusCode -eq 200)
         {
@@ -48,39 +48,39 @@ function resetRestrictInternet()
 #check command line for passed in arguments if they equal testInternetAccess or enableRestrictInternet or resetRestrictInternet run coresponding function
 if($args[0] -eq "testInternetAccess")
 {
-    echo "testing internet access"
+    Write-Output "testing internet access"
     #run testInternetAccess function
     if(testInternetAccess -eq $true)
     {
-        echo "Web response 200 iternet connection successful"
+        Write-Output "Web response 200 iternet connection successful"
     }else
     {
-        echo "Web request failed no internet connection"
+        Write-Output "Web request failed no internet connection"
     }
 }elseIf($args[0] -eq "enableRestrictInternet")
 {
-    echo "restricting internet access"
+    Write-Output "restricting internet access"
     enableRestrictInternet
     if(testInternetAccess -eq $true)
     {
-        echo "error: Web response 200 internet still avaliable"
-        echo "please contact your administrator"
+        Write-Output "error: Web response 200 internet still avaliable"
+        Write-Output "please contact your administrator"
     }else
     {
-        echo "success: no internet connection"
+        Write-Output "success: no internet connection"
     }
 }elseIf($args[0] -eq "resetRestrictInternet")
 {
-    echo "re-enabling internet"
+    Write-Output "re-enabling internet"
     resetRestrictInternet
     if(testInternetAccess -eq $true)
     {
-        echo "success: Web response 200 iternet connection successful"
+        Write-Output "success: Web response 200 iternet connection successful"
     }else
     {
-        echo "Web request failed no internet connection"
+        Write-Output "Web request failed no internet connection"
     }
 }else
 {
-    echo "Please enter a valid option: testInternetAccess or enableRestrictInternet or resetRestrictInternet"
+    Write-Output "Please enter a valid option: testInternetAccess or enableRestrictInternet or resetRestrictInternet"
 }
